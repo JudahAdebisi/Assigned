b8a006bf8d4494434d36ce339e7f866ffbb03d3a
JudahAdebisi <Judahadebisi1@gmail.com>
2024-07-06 13:50:07 -0400

    This is a python code asking the user of their favorite food and printing it out

---
+++ food.py
@@ -0,0 +1,7 @@
+# Ask the user for their favorite food
+food = input("What is your favorite food? ")
+
+# Print a message including the users favorite food
+
+print(f"Your favorite food is {food}!")
+
