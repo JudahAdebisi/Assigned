a4da7483da1cb22054dac858a56c4a1d0a6071c8
JudahAdebisi <Judahadebisi1@gmail.com>
2024-07-06 17:01:42 -0400

    This is a python code allowing the user to tell the system a little bit about themselves

---
+++ name_age_color.py
@@ -0,0 +1,7 @@
+# Ask the user of their name, age, and color
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+color = input("What is your favorite color? ")
+
+# Print a short summary of the user
+print(f"Your name is {name} and your are {age} and like the color {color}")
