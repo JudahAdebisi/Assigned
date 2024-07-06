9614ba0a6bcf632326552e8cfcda1273dbf3a9d1
JudahAdebisi <Judahadebisi1@gmail.com>
2024-07-06 13:37:19 -0400

    This is a python code askiing the user for their name and age then printing it out

---
+++ name.py
@@ -0,0 +1,7 @@
+# Ask the user for name and age
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+
+# Print a message adding the users name and age
+
+print(f"Your name is {name} and you are {age} years old")
