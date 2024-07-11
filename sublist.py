3b1e5185ab021eea485f01cdd6626874283d0928
JudahAdebisi <Judahadebisi1@gmail.com>
2024-07-10 23:51:03 -0400

    This is a python code that prints the sum of each sublist

---
+++ sublist.py
@@ -0,0 +1,11 @@
+# Create a list of lists with numbers
+list_of_lists = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12]
+]
+
+# Print out the sum of each sublist
+for sublist in list_of_lists:
+    print("Sum of sublist:", sum(sublist))
