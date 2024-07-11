f5f669e7d37b2740b40169d38d5bfdefb11a6ac3
JudahAdebisi <Judahadebisi1@gmail.com>
2024-07-10 20:29:03 -0400

    This is a python code that prints out the second-largest number

---
+++ list_numbers.py
@@ -0,0 +1,9 @@
+# Create a list of numbers
+numbers = [12, 45, 7, 23, 56, 89, 34, 1]
+
+# Sort the list in descending order
+numbers.sort(reverse=True)
+
+# Print the second-largest number
+print("The second-largest number is:", numbers[1])
+
