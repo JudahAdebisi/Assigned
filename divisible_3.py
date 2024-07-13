378a1e2c03840b73bb999043006aff6129f0b158
JudahAdebisi <Judahadebisi1@gmail.com>
2024-07-12 20:43:56 -0400

    This is a python code that print the numbers that are divisible by 3 in the list

---
+++ divisible_3.py
@@ -0,0 +1,9 @@
+
+# Create a list of numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Use a list comprehension to find numbers divisible by 3
+divisible_by_3 = [num for num in numbers if num % 3 == 0]
+
+# Print the numbers divisible by 3
+print("Numbers divisible by 3:", divisible_by_3)
